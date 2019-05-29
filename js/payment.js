3f6509535ac4367908d9d138e89c370590130740
aintelligen <aintelligen@163.com>
2019-05-29 15:56:04 +0800

    feat：init

---
+++ js/payment.js
@@ -0,0 +1,39 @@
+$(function() {
+  var payment = {
+    init: function() {
+      var price = public.localStorage.get('price');
+      $('.s-detail_a .s-price').text(price);
+      $('.s-money_a .s-price').text('￥' + price);
+      $('.s-money_b .s-price').text('￥' + price);
+      $('[event-payment]').on('click', function() {
+        //获取code
+        // public.getCode();
+        var appid = 'wx2ad7e44b51a54aa1';
+        //获取 code
+        var local = window.location.href;
+        var code = getParam('code');
+        if (code === null || code === '') {
+          // 跳转至授权地址，该地址只支持微信浏览器打开
+          window.location.href =
+            'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' +
+            appid +
+            '&redirect_uri=' +
+            encodeURIComponent(local) +
+            '&response_type=code&scope=snsapi_base#wechat_redirect';
+        } else {
+          console.log(code);
+        }
+      });
+      if (public.getParameter('code')) {
+        //判断有没有code
+        var code = public.getParameter('code');
+        var orderId = public.localStorage.get('orderId');
+        var backUrl = h5Url + 'paymentSuccess.html?orderId=' + orderId;
+        var orderNo = public.localStorage.get('orderNo');
+        public.getOpenid(code);
+        public.onBridgeReady(orderNo, backUrl);
+      }
+    }
+  };
+  payment.init();
+});
